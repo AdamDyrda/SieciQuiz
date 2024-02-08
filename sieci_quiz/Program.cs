5d89860c524bd7b0ad7ef715a2e62c45cd58cce0
Adam Dyrda <a.dyrda102@gmail.com>
2024-02-08 12:27:01 +0100

    Update of questions database and small overall improvements

--- sieci_quiz/Program.cs
+++ sieci_quiz/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics.Metrics;
+using System.IO;
 namespace sieci_quiz;
 
 class Program
@@ -14,13 +15,18 @@ class Program
             Pytania.Add(part);
         }
 
+        int QuestionsCount = Pytania.Count();
+        int Counter = new int();
         Random rnd = new Random();
         while (Pytania.Count > 0)
         {
             PrintQuestion(rnd.Next(0, Pytania.Count), Pytania);
+            Counter++;
             Console.Clear();
 
         }
+        Console.WriteLine("Odpowiedziales poprawnie na wszystkie pytania");
+        Console.WriteLine($"Ilosc powtorzonych pytan to: {Counter-QuestionsCount}");
     }
 
     static void PrintQuestion(int questionNumber, List<string> Pytania)
@@ -33,12 +39,14 @@ class Program
         }
 
         Console.Write("Odpowiedz: ");
-        if (data[1].Trim() == Console.ReadLine().ToUpper())
+        if (data[1].Trim() == Console.ReadLine().ToUpper().Trim())
         {
+            
             Console.Clear();
             Console.WriteLine("Prawidlowa odpowiedz! Nacisnij przycisk aby przejsc dalej...");
             Pytania.RemoveAt(questionNumber);
             Console.ReadKey();
+            
         }
         else
         {
